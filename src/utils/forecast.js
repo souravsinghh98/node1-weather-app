a98792299f5297d5dd63ed050a764e4acc6f8378
unknown <Sourav Singh>
2019-08-05 13:17:50 +0530

    Add index page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,6 +2,7 @@ const request=require('request')
 
 const forecast=(latitude,longitude,callback)=>{
     const url='https://api.darksky.net/forecast/76e9efdb66bd8c22f5df8030fbd31ebe/'+latitude+','+longitude;
+    console.log(latitude+' ,'+longitude)
     request({url:url,json:true},(error,response)=>{
         if(error)
         {
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request=require('request')
 
 const geocode=(address,callback)=>{
-    const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1Ijoic291cmF2MTk5OCIsImEiOiJjanlvNGY4Zm4xMHd5M2RvOWVvYXd3YTIzIn0.F9x_P9Ymy22713UJbJyZKA&limit=1'
+    const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1Ijoic291cmF2MTk5OCIsImEiOiJjanlvNGY4Zm4xMHd5M2RvOWVvYXd3YTIzIn0.F9x_P9Ymy22713UJbJyZKA'
     request({url:url,json:true},(error,response)=>{
       if(error)
       {
